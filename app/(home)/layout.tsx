53a35220bc0211d24beb255d7378528b64f0adaf
texiwustion <w6666s@outlook.com>
2024-07-09 16:35:56 +0800

    [add] swr wrapper

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,4 +1,5 @@
 import MainMenu from "./components/main-menu";
+import { SWRProvider } from "@/components/providers/swr-provider";
 
 interface ILayout {
   children: React.ReactNode;
@@ -8,7 +9,9 @@ export default function DashboardLayout({ children }: ILayout) {
   return (
     <div className="grid grid-cols-[250px_1fr] h-screen">
       <MainMenu />
-      <div className="overflow-auto p-4">{children}</div>
+      <div className="overflow-auto p-4">
+        <SWRProvider>{children}</SWRProvider>
+      </div>
     </div>
   );
 }
---
+++ components/providers/swr-provider.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { SWRConfig } from "swr";
+export const SWRProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SWRConfig value={{ fetcher: url => fetch(url).then(res => res.json()) }}>
+      {children}
+    </SWRConfig>
+  );
+};
