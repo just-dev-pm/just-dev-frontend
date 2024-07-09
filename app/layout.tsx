7cb627f1c378f8c477e707b89447a571a718bf16
texiwustion <w6666s@outlook.com>
2024-07-09 20:37:28 +0800

    [add] 添加全局 toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,25 +2,29 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const poppins = Poppins({
-	subsets: ["latin"],
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
-	title: "Just Dev",
-	description: "Simple but powerful project manager.",
+  title: "Just Dev",
+  description: "Simple but powerful project manager.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body className={cn(poppins.className)}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={cn(poppins.className)}>
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
 }
