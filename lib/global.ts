efd4a3555c1e6e435e9322d1b4705eb808571dfc
texiwustion <w6666s@outlook.com>
2024-07-10 15:43:35 +0800

    [add] 多运行环境支持

--- lib/global.ts
+++ lib/global.ts
@@ -1,3 +1,4 @@
-export const mockBaseUrl = "http://127.0.0.1:4523/m1/4779953-0-default"
-export const testBaseUrl = "http://localhost:3000"
-export const hosturl = 'ws://localhost:1234'
+export const mockBaseUrl = "http://127.0.0.1:4523/m1/4779953-0-default";
+export const testBaseUrl = "http://localhost:3000";
+export const hosturl = "ws://localhost:1234";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
