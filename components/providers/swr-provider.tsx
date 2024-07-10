657a6ec96a64def7b04fcf690ecb6b6270abb0b6
texiwustion <w6666s@outlook.com>
2024-07-10 10:19:07 +0800

    [upd] get with params

--- components/providers/swr-provider.tsx
+++ components/providers/swr-provider.tsx
@@ -2,7 +2,12 @@
 import { SWRConfig } from "swr";
 export const SWRProvider = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SWRConfig value={{ fetcher: url => fetch(url).then(res => res.json()) }}>
+    <SWRConfig
+      value={{
+        fetcher: ([url, options]) =>
+          fetch(url, options).then(res => res.json()),
+      }}
+    >
       {children}
     </SWRConfig>
   );
