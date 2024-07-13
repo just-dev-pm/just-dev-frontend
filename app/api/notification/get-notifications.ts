dca7797aedd813d0cb92c1f0055f0be87f5b98eb
Crush-0 <111955144+Crush-0@users.noreply.github.com>
2024-07-13 09:06:47 +0800

    实现需求部分api

---
+++ app/api/notification/get-notifications.ts
@@ -0,0 +1,30 @@
+import { BASE_URL } from "@/lib/global";
+import useSWR from "swr";
+
+/** @key [/api/users/,{user_id},/notifications] */
+
+export default function useNotifications({ user_id }: { user_id: string }) {
+  const urlPrefix = `/api/users/`;
+  const urlSuffix = `/notifications`;
+  const { data, error } = useSWR(
+    user_id ? [urlPrefix, user_id, urlSuffix] : null,
+    ([urlPrefix, user_id, urlSuffix]) =>
+      fetch(BASE_URL + urlPrefix + user_id + urlSuffix, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+        credentials: "include",
+      }).then((res) => {
+        if (!res.ok) {
+          throw new Error(`Error! Status:${res.status}`);
+        }
+        return res.json();
+      }),
+    { suspense: true }
+  );
+  return {
+    data,
+    error,
+  };
+}
